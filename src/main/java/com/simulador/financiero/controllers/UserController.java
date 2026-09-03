d6f9949289b4b43495946be6a0cff55605288c62
Urcelay <128077803+Urcelay@users.noreply.github.com>
2026-09-03 18:13:48 +0000

    user_update

--- src/main/java/com/simulador/financiero/controllers/UserController.java
+++ src/main/java/com/simulador/financiero/controllers/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simulador.financiero.entities.UserEntity;
 import com.simulador.financiero.entities.UserResponse;
+import com.simulador.financiero.repositories.UserRepository;
 import com.simulador.financiero.services.UserService;
 
 import jakarta.validation.Valid;
@@ -78,6 +80,29 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserEntity user) {
+
+        UserEntity usuarioActualizado =
+                userService.updateUser(id, user);
+
+        if (usuarioActualizado == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        UserResponse response = new UserResponse(
+                usuarioActualizado.getId(),
+                usuarioActualizado.getFullName(),
+                usuarioActualizado.getEmail(),
+                usuarioActualizado.getStatus().name(),
+                usuarioActualizado.getCreatedAt()
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
