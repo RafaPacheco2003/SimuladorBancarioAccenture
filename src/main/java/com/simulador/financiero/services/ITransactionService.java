e2fe8f58e11386081f39a312ad6d7cb800e57d8f
HectorVargas00 <vargasolivares@gmail.com>
2026-09-17 18:49:59 +0000

    Historial de Transferencias

--- src/main/java/com/simulador/financiero/services/ITransactionService.java
+++ src/main/java/com/simulador/financiero/services/ITransactionService.java
@@ -2,8 +2,13 @@ package com.simulador.financiero.services;
 
 import com.simulador.financiero.DTOs.request.TransactionRequest;
 import com.simulador.financiero.DTOs.response.ComprobanteResponse;
+import com.simulador.financiero.DTOs.response.TransactionHistResponse;
+
+import java.util.List;
 
 public interface ITransactionService {
 
   public ComprobanteResponse performTransaction(Long userId, TransactionRequest request);
+
+  List<TransactionHistResponse> getTransactionsHistory(String accountNumber);
 }
